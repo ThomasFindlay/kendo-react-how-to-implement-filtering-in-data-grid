75ce78b0a004253a0ecb21e3fe36d97ece6dd0e6
Thomas Findlay <thomasfindlay94@gmail.com>
2023-04-11 11:09:33 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import "./App.css";
-import RecipesTable from "./components/RecipesTable.jsx";
-import RecipesGridWithColumnFilters from "./components/RecipesGridWithColumnFilters.jsx";
-import RecipesGridWithCustomCellFilter from "./components/RecipesGridWithCustomCellFilter.jsx";
-import RecipesGridWithColumnMenuFilters from "./components/RecipesGridWithColumnMenuFilters";
 
 function App() {
   return (
     <div className="App">
       <h1>KendoReact Data Grid Filtering</h1>
-      {/* <h2>Default Table</h2>
-      <RecipesTable /> */}
-      {/* <h2>Grid with column filters</h2>
-      <RecipesGridWithColumnFilters /> */}
-      {/* <h2>Grid with custom column cell filter</h2>
-      <RecipesGridWithCustomCellFilter /> */}
-      <h2>Grid with column menu filters</h2>
-      <RecipesGridWithColumnMenuFilters />
     </div>
   );
 }
